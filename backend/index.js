2e9a5f504169e51549602f7cf14321531597db87
YASH <74112721+redwild1066@users.noreply.github.com>
2022-11-24 21:16:29 +0530

    app pins routes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ mongoose
  .then(() => console.log("MongoDB connected!"))
  .catch(err => console.log(err));
 
-
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
