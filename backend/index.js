5a422af31259511609b6397a8680db9dd0e942be
YASH <74112721+redwild1066@users.noreply.github.com>
2022-11-24 21:16:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,7 @@ mongoose
  .then(() => console.log("MongoDB connected!"))
  .catch(err => console.log(err));
 
-app.use("/api/users", userRoute);
-app.use("/api/pins", pinRoute);
+
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
