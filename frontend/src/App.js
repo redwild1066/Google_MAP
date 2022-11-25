96a61d85c5144a54290e2473f2d7bcb50c728a98
jinay1143 <84394365+jinay1143@users.noreply.github.com>
2022-11-25 17:23:41 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,43 @@
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, {
+  Marker,
+  NavigationControl,
+  Popup,
+  FullscreenControl,
+  GeolocateControl,
+} from "react-map-gl";
+import { useState } from "react";
+function App() {
+  const [lng, setLng] = useState(54.37585762735543);
+  const [lat, setLat] = useState(24.45677614934833);
 
+  return (
+    <div className="App">
+    {console.log(process.env.REACT_APP_MAP_KEY)}
+      <h1>Mapbox tutorial</h1>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
+        style={{
+          width: "500px",
+          height: "500px",
+          borderRadius: "15px",
+          border: "2px solid red",
+        }}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} />
+        <NavigationControl position="bottom-right" />
+        <FullscreenControl />
+
+        <GeolocateControl />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
